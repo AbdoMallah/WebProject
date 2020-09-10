413899a00495c39ee2fd861f840bbf62b492bd32
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2020-09-10 12:07:32 +0200

    done with html&css

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express') //TO USE express frameWork
 const expresshandlebars = require('express-handlebars') //To Use to handlebars in express
 const app = express()
-const port = 3000
+var http = require('http')
+var fs = require('fs')
+const port = 8000
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
 
-app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${'+ port + '}')
-})
+function onRequest(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('../index.html', null, function(error, data){
+        if(error){
+        response.writeHead(404)
+        response.write("File not found! :C")}
+        else{
+            response.write(data)
+        }
+        response.end()
+    })
+}
+http.createServer(onRequest).listen(port)
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+
+// app.listen(port, () => {
+//     console.log('Example app listening at http://localhost:${'+ port + '}')
+// })
--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('../about.html', function(err, data) {
+  console.log("Hi")
+  fs.readFile('../index.html', function(err, data) {
+    console.log("Hi again")
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
