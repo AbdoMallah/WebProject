9c56ae1f16b23b4cd2e15dd575439b3d7a5a0d5e
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2020-09-17 14:51:09 +0200

    Html to hbs

--- app.js
+++ app.js
@@ -6,25 +6,53 @@ var fs = require('fs')
 const port = 8000
 
 
-function onRequest(request, response){
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('../index.html', null, function(error, data){
-        if(error){
-        response.writeHead(404)
-        response.write("File not found! :C")}
-        else{
-            response.write(data)
-        }
-        response.end()
-    })
-}
-http.createServer(onRequest).listen(port)
+
+app.engine('hbs', expresshandlebars({
+    defaultLayout: 'main.hbs'
+}))
+
+app.use(express.static('node_modules/spectre.css/dist'))
+app.use(express.static('CSS'))
+app.use(express.static('views/images')) 
+/* ===== Render ===== */
+app.get('/', (req, res) => {
+    res.render('index.hbs')
+})
+app.get('/login', (req, res) => {
+    res.render('login.hbs')
+})
+app.get('/contact', (req, res) => {
+    res.render('contact.hbs')
+})
+app.get('/setting', (req, res) => {
+    res.render('setting.hbs')
+})
+app.get('/post', (req, res) => {
+    res.render('post.hbs')
+})
+app.get('/about', (req, res) => {
+    res.render('about.hbs')
+})
+
+app.listen(port);
 
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World')
-// })
 
-// app.listen(port, () => {
-//     console.log('Example app listening at http://localhost:${'+ port + '}')
-// })
+
+
+
+
+
+// function onRequest(request, response){
+//     response.writeHead(200, {'Content-Type': 'text/html'})
+//     fs.readFile('./index.html', null, function(error, data){
+//         if(error){
+//         response.writeHead(404)
+//         response.write("File not found! :C")}
+//         else{
+//             response.write(data)
+//         }
+//         response.end()
+//     })
+// }
+// http.createServer(onRequest).listen(port)
