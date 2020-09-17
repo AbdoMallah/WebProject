d538e648769bcaefff12bb3c8a6c95222714a324
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2020-09-17 20:50:06 +0200

    DB Function

--- DB/db.js
+++ DB/db.js
@@ -1,12 +1,11 @@
 const sqlite3 = require('sqlite3')
-let db = new sqlite3.Database("database .db") //Test 1
-var userQuery = "CREATE TABLE IF NOT EXISTS users ( Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName TEXT NOT NULL, LastName TEXT NOT NULL, Email TEXT NOT NULL UNIQUE, Password TEXT NOT NULL, PhoneNumber NUMBER NOT NULL UNIQUE)"
+let db = new sqlite3.Database("database.db") 
 var postQuery = "CREATE TABLE IF NOT EXISTS posts ( Id INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT NOT NULL, Description TEXT NOT NULL, Prise NUMBER NOT NULL, Image TEXT NOT NULL,  PosterID NUMBER INTEGER , FOREIGN KEY (PosterID) REFERENCES users (Id))";
 
 
-/* === Test 1 === */ 
-db.run(userQuery, function(error){
+
+/* === Create Table if not exist  === */ 
+db.run(postQuery, function(error){
     if(error){  return console.error(error.message);    }
     else{   console.log("Query Successfully exectued"); }
 })
-// db.run(postQuery)
--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 const express = require('express') //TO USE express frameWork
 const expresshandlebars = require('express-handlebars') //To Use to handlebars in express
 const app = express()
+const sqlite3 = require('sqlite3')
 var http = require('http')
 var fs = require('fs')
 const port = 8000
+let db = new sqlite3.Database("database.db") 
 
+/* === DataBase === */ 
+function createTable(){
+    var postQuery = "CREATE TABLE IF NOT EXISTS posts ( Id INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT NOT NULL, Description TEXT NOT NULL, Prise NUMBER NOT NULL, Image TEXT NOT NULL,  PosterID NUMBER INTEGER , FOREIGN KEY (PosterID) REFERENCES users (Id))";
+    db.run(postQuery, function(error){
+        if(error){  return console.error(error.message);    }
+        else{   console.log("Query Successfully exectued"); }
+    })
+}
 
+function insertPostInfo(Title, Description, Prise, Image, PosterID){
+    var insertQuery = "INSERT INTO posts('Title', 'Description', 'Prise', 'Image', 'PosterID') VALUE ("+ Title +", "+ Description +","+ Prise +"," + Image +","+ PosterID + ")";
+    db.run(insertQuery, function(error){
+        if(error){  return console.error(error.message);    }
+        else{   console.log("Query Successfully exectued"); }
+    })
+}
+function selectPosts(){
+    var selectQuery = "SELECT * FROM posts";
+    db.run(selectQuery, function(error){
+        if(error){  return console.error(error.message);    }
+        else{   console.log("Query Successfully exectued"); }
+    })
+}
+function selectPost(Id){
+    var selectQuery = "SELECT * FROM posts WHERE Id ==" + Id;
+    db.run(selectQuery, function(error){
+        if(error){  return console.error(error.message);    }
+        else{   console.log("Query Successfully exectued"); }
+    })
+}
+function updatePost(Id, Title, Description, Prise, Image){
+    var selectQuery = "UPDATE posts SET Title = "+Title+", Description = "+Description +", Prise = "+Prise+",Image = "+Image+"  WHERE Id ==" + Id;
+    db.run(selectQuery, function(error){
+        if(error){  return console.error(error.message);    }
+        else{   console.log("Query Successfully exectued"); }
+    })
+}
 
+/* === Express-Handlebars === */ 
 app.engine('hbs', expresshandlebars({
     defaultLayout: 'main.hbs'
 }))
