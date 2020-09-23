a81e7a39093088cd24892e60478481d08d25c934
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2020-09-24 01:53:28 +0200

    Test

--- app.js
+++ app.js
@@ -81,21 +81,3 @@ app.listen(port);
 
 
 
-
-
-
-
-
-// function onRequest(request, response){
-//     response.writeHead(200, {'Content-Type': 'text/html'})
-//     fs.readFile('./index.html', null, function(error, data){
-//         if(error){
-//         response.writeHead(404)
-//         response.write("File not found! :C")}
-//         else{
-//             response.write(data)
-//         }
-//         response.end()
-//     })
-// }
-// http.createServer(onRequest).listen(port)
