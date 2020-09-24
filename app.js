011378185df82fbd7c8f329c5b63366091ae6c28
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2020-09-24 19:49:46 +0200

    login--Done
    Done with Login

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express') //TO USE express frameWork
 const expresshandlebars = require('express-handlebars') //To Use to handlebars in express
 const app = express()
 const sqlite3 = require('sqlite3')
+const bcrypt = require('bcrypt') //uses to hash password, don't need it in is Projekt. 
 var http = require('http')
 var fs = require('fs')
 const port = 8080
 let db = new sqlite3.Database("database.db") 
 
+
+const USERNAME = "Admin";
+const PASSWORD = "20aDmIn20";
+var status = 1;
 /* === DataBase === */ 
 function createTable(){
     var postQuery = "CREATE TABLE IF NOT EXISTS posts ( Id INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT NOT NULL, Description TEXT NOT NULL, Prise NUMBER NOT NULL, Image TEXT NOT NULL)";
@@ -25,26 +30,26 @@ function insertPostInfo(Title, Description, Prise, Image){
 }
 function selectPosts(){
     var selectQuery = "SELECT * FROM posts";
-    db.run(selectQuery, function(error){
+    db.run(selectQuery, function(error,data){
         if(error){  return console.error(error.message);    }
-        else{   console.log("Query Successfully exectued"); }
+        else{   console.log("Query Successfully exectued");  }
     })
 }
 function selectPost(Id){
     var selectQuery = "SELECT * FROM posts WHERE Id ==" + Id;
-    db.run(selectQuery, function(error){
+    db.run(selectQuery, function(error,data){
         if(error){  return console.error(error.message);    }
         else{   console.log("Query Successfully exectued"); }
     })
 }
 function updatePost(Id, Title, Description, Prise, Image){
     var selectQuery = "UPDATE posts SET Title = " ||Title|| ", Description = "|| Description ||", Prise = "|| Prise ||",Image = "||Image||"  WHERE Id ==" || Id;
-    db.run(selectQuery, function(error){
+    db.run(selectQuery, function(error,data){
         if(error){  return console.error(error.message);    }
         else{   console.log("Query Successfully exectued"); }
     })
 }
-
+var SPosts = selectPosts();
 createTable();
 insertPostInfo('Test2', 'hehek jieu e erea', '400', 'test1.jpg');
 
@@ -58,23 +63,34 @@ app.use(express.static('node_modules/spectre.css/dist'))
 app.use(express.static('CSS'))
 app.use(express.static('views/images')) 
 /* ===== Render ===== */
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', (req, res) => {
-    res.render('index.hbs')
+    res.render('index.hbs', {status})
 })
 app.get('/login', (req, res) => {
+    if(status == 0){
+        req.redirect('/')
+    }
     res.render('login.hbs')
 })
-app.get('/contact', (req, res) => {
-    res.render('contact.hbs')
+app.post('/login', (req, res) => {
+    if(req.body.Username == USERNAME && req.body.Password == PASSWORD){
+        status = 0;
+        res.redirect('/')
+    }
 })
-app.get('/setting', (req, res) => {
-    res.render('setting.hbs')
+app.get('/contact', (req, res) => {
+    res.render('contact.hbs', {status})
+})
+app.get('/upload', (req, res) => {
+    res.render('upload.hbs', {status})
 })
 app.get('/post', (req, res) => {
-    res.render('post.hbs')
+    res.render('post.hbs', {status})
 })
 app.get('/about', (req, res) => {
-    res.render('about.hbs')
+    res.render('about.hbs', {status})
 })
 
 app.listen(port);
