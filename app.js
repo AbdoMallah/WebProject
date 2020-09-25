c47802d3c2ad3011bcff2e5a1c4fd24724721900
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2020-09-25 19:48:49 +0200

    Login-logout Done with sessions

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express') //TO USE express frameWork
 const expresshandlebars = require('express-handlebars') //To Use to handlebars in express
+const session = require('express-session')
+const cookieParser  = require('cookie-parser')
 const app = express()
 const sqlite3 = require('sqlite3')
 const bcrypt = require('bcrypt') //uses to hash password, don't need it in is Projekt. 
 var http = require('http')
 var fs = require('fs')
-const port = 8080
-let db = new sqlite3.Database("database.db") 
+const port = 8000
+let db = new sqlite3.Database("database.db")
 
-
+/* === Admin === */
 const ADMIN = "Admin";
-const PASSWORD = "20aDmIn20";
-var customer = true;
+const PASSWORD = "admin";
 /* === DataBase === */ 
 function createTable(){
     var postQuery = "CREATE TABLE IF NOT EXISTS posts ( Id INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT NOT NULL, Description TEXT NOT NULL, Prise NUMBER NOT NULL, Image TEXT NOT NULL)";
@@ -20,7 +21,6 @@ function createTable(){
         else{   console.log("Query Successfully exectued"); }
     })
 }
-
 function insertPostInfo(Title, Description, Prise, Image){
     var insertQuery = "INSERT INTO posts('Title', 'Description', 'Prise', 'Image') VALUES ('" + Title + "', '" + Description + "','" + Prise + "','" + Image + "')";
     db.run(insertQuery, function(error){
@@ -49,9 +49,9 @@ function updatePost(Id, Title, Description, Prise, Image){
         else{   console.log("Query Successfully exectued"); }
     })
 }
-var SPosts = selectPosts();
-createTable();
-insertPostInfo('Test2', 'hehek jieu e erea', '400', 'test1.jpg');
+// var SPosts = selectPosts();
+// createTable();
+// insertPostInfo('Test2', 'hehek jieu e erea', '400', 'test1.jpg');
 
 /* === Express-Handlebars === */ 
 app.engine('hbs', expresshandlebars({
@@ -59,38 +59,61 @@ app.engine('hbs', expresshandlebars({
     layoutsDir: __dirname + '/views/layouts/'
 }))
 
+    
 app.use(express.static('node_modules/spectre.css/dist'))
 app.use(express.static('CSS'))
 app.use(express.static('views/images')) 
-/* ===== Render ===== */
 app.use(express.urlencoded({extended: false}))
+app.use(session({
+    saveUninitialized: false,
+    resave: false,
+    secret: 'hehahrhahe'
+}))
 
-app.get('/', (req, res) => {
-    res.render('index.hbs', {customer})
-})
-app.get('/login', (req, res) => {
-    if(customer == false){
-        req.redirect('/')
-    }else{ res.render('login.hbs', {customer}) }
-})
-app.post('/login', (req, res) => {
+app.use(function(req,res,next){
+    const isLoggedIn = req.session.isLoggedIn
+    res.locals.isLoggedIn = isLoggedIn
+    next()
+  })
+
+/* ===== POST ===== */
+app.post('/login', (req, res ) => {
     if(req.body.Username == ADMIN && req.body.Password == PASSWORD){
-        customer = 0;
+        req.session.isLoggedIn = true;
         res.redirect('/')
     }
 })
+app.post('/logOut', (req, res) => {
+    if(req.session.isLoggedIn == true){
+        req.session.isLoggedIn = false;
+        res.redirect('/')
+    }
+    res.redirect('/')
+})
 
+/* ===== GET ===== */ 
+app.get('/', (req, res) => {
+    res.render('index.hbs')
+})
+app.get('/login', (req, res) => {
+    if(req.session.isLoggedIn == true){
+        res.redirect('/')
+    }else{ res.render('login.hbs') }
+})
 app.get('/contact', (req, res) => {
-    res.render('contact.hbs', {customer})
+    res.render('contact.hbs')
 })
 app.get('/upload', (req, res) => {
-    res.render('upload.hbs', {customer})
+    if(req.session.isLoggedIn){
+        res.render('upload.hbs')
+    }res.redirect('/')
 })
+/*Check the link later*/ 
 app.get('/post', (req, res) => {
-    res.render('post.hbs', {customer})
+    res.render('post.hbs')
 })
 app.get('/about', (req, res) => {
-    res.render('about.hbs', {customer})
+    res.render('about.hbs')
 })
 
 app.listen(port);
